39670c9df2bce8ad0415a56bac5ea1a370c4ffaa
MarcoErice <marco.erice@gmail.com>
2017-11-15 13:58:01 +0100

    push

--- DatabaseCreator/Program.cs
+++ DatabaseCreator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,57 @@ namespace DatabaseCreator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ange namn på nytt databas:");
+            try
+            {
+                var databaseName = Console.ReadLine();
+                CreateDatabase(databaseName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Namet på database finns redan\nTryck Enter för och fortsätta");
+                Console.ReadLine();
+                //throw;
+
+            }
+            
+            //Console.WriteLine("Skriv tabell namn:");
+            var TableName = Console.ReadLine();
+            
+            //OpenEducation();
+            //CreateTables(TableName);
+            Console.ReadLine();                       
+        }
+
+        
+        private static void CreateDatabase(string databaseName)
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=Master";
+            SqlConnection cn = new SqlConnection(cns);
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "CREATE DATABASE [" + databaseName + "]";
+            cmd.ExecuteNonQuery();
+            Console.WriteLine(databaseName);
+            cn.Close();
+
+        }
+
+        private static SqlConnection OpenEducation()
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=Education";
+            SqlConnection cn = new SqlConnection(cns);
+            return cn;
+            
+        }
+        private static void CreateTables(string name)
+        {
+            var cn = OpenEducation();
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "CREATE TABLE [dbo].["+name+"] (["+name+"ID] [int] not null primary key identity(1,1), ["+name+"] [nvarchar](15) null)";
+
+            cn.Close();
         }
     }
 }
