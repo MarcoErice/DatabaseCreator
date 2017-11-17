426f6a5bcebe0489fe73810e8bc335c0ef44ee46
MarcoErice <marco.erice@gmail.com>
2017-11-17 01:33:56 +0100

    Done, except ListTableContents()

--- DatabaseCreator/Program.cs
+++ DatabaseCreator/Program.cs
@@ -9,60 +9,124 @@ namespace DatabaseCreator
 {
     class Program
     {
-        static string databaseName;
         static void Main(string[] args)
         {
-            Console.WriteLine("Ange namn på nytt databas:");
-            try
+            ShowMenu();
+            var choice = Console.ReadLine();
+            while (choice != "A")
             {
-                databaseName = Console.ReadLine();
-                CreateDatabase(databaseName);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Namet på database finns redan\nTryck Enter för och fortsätta");
-                Console.ReadLine();
-                //throw;
+                switch (choice)
+                {
+                    case "1":
+                        CreateDatabase();
+                        break;
+                    case "2":
+                        CreateCoursesTable();
+                        break;
+                    case "3":
+                        CreateStudentsTable();
+                        break;
+                    case "4":
+                        CreateCourseStudentsTable();
+                        break;
+                    case "5":
+                        FillTablesWithData();
+                        break;
+                    case "6":
+                        ListTableContents();
+                        break;
 
+                }
+                Console.Beep();
+                ShowMenu();
+                choice = Console.ReadLine();
             }
-            
-            //Console.WriteLine("Skriv tabell namn:");
-            var TableName = Console.ReadLine();
-            
-            //OpenEducation();
-            //CreateTables(TableName);
-            Console.ReadLine();                       
+                     
         }
 
-        
-        private static void CreateDatabase(string databaseName)
+        private static void ShowMenu()
+        {
+            Console.WriteLine("1. Skapa databasen Education");
+            Console.WriteLine("2. Lägg till tabellen Courses");
+            Console.WriteLine("3. Lägg till tabellen Students");
+            Console.WriteLine("4. Lägg till tabellen CourseStudents");
+            Console.WriteLine("5. Fyll tabellerna med data");
+            Console.WriteLine("6. Lista innehållet i tabellerna");
+            Console.WriteLine("A. Avsluta");
+        }
+        private static void CreateDatabase()
         {
             var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=Master";
             SqlConnection cn = new SqlConnection(cns);
             cn.Open();
             var cmd = cn.CreateCommand();
-            cmd.CommandText = "CREATE DATABASE [" + databaseName + "]";
-            cmd.ExecuteNonQuery();
-            Console.WriteLine(databaseName);
+            cmd.CommandText = "CREATE DATABASE [EducationX]";
+            cmd.ExecuteNonQuery();            
             cn.Close();
-
         }
-
-        private static SqlConnection OpenEducation()
+        private static void CreateCoursesTable()
         {
-            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database="+databaseName+"";
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=EducationX";
             SqlConnection cn = new SqlConnection(cns);
-            return cn;
-            
-        }
-        private static void CreateTables(string name)
-        {
-            var cn = OpenEducation();
             cn.Open();
             var cmd = cn.CreateCommand();
-            cmd.CommandText = "CREATE TABLE [dbo].["+name+"] (["+name+"ID] [int] not null primary key identity(1,1), ["+name+"] [nvarchar](15) null)";
-
+            cmd.CommandText = "CREATE TABLE[dbo].[Courses] ([CourseID][int] IDENTITY(1,1) NOT NULL, [Name] [nvarchar] (30) NOT NULL)";
+            cmd.ExecuteNonQuery();
             cn.Close();
         }
+        private static void CreateStudentsTable()
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=EducationX";
+            SqlConnection cn = new SqlConnection(cns);
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "CREATE TABLE[dbo].[Students] ([StudentID][int] IDENTITY(1,1) NOT NULL, [Name] [nvarchar] (30) NOT NULL)";
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+        private static void CreateCourseStudentsTable()
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=EducationX";
+            SqlConnection cn = new SqlConnection(cns);
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "CREATE TABLE [dbo].[CourseStudents]([StudentCourseID][int] IDENTITY(1, 1) NOT NULL, [CourseID] [int] NOT NULL, [StudentID] [int] NOT NULL, CONSTRAINT[PK_CourseStudents] PRIMARY KEY CLUSTERED ([StudentCourseID]))";
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+        private static void FillTablesWithData()
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=EducationX";
+            SqlConnection cn = new SqlConnection(cns);
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            for (var studentNo = 0; studentNo < 100; studentNo++)
+            {
+                cmd.CommandText = $"INSERT INTO Students (Name) VALUES ('{studentNo.ToString()}')";
+                cmd.ExecuteNonQuery();
+            }
+            for (var courseNo = 0; courseNo < 100; courseNo++)
+            {
+                cmd.CommandText = $"INSERT INTO Courses (Name) VALUES ('{courseNo.ToString()}')";
+                cmd.ExecuteNonQuery();
+            }
+            cn.Close();
+        }
+        private static void ListTableContents()
+        {
+            var cns = "Data Source=(localdb)\\MSSQLLocalDB;Database=EducationX";
+            SqlConnection cn = new SqlConnection(cns);
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "SELECT TOP (5) Name FROM Students;";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+            reader.Close();
+            cn.Close();
+        }
+
     }
 }
